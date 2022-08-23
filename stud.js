043ddeb73e618c38c3eca7d655ee50d6f5373ac3
AiswaryaAjayakumar <aiswaryaajayakumar003@gmail.com>
2022-08-23 10:11:49 +0530

    docker

--- stud.js
+++ stud.js
@@ -6,6 +6,9 @@ var mongoose=require('mongoose')
 const { loginModel } = require('./models/login')
 const { profileModel } = require('./models/profile')
 
+
+mongoose.connect("mongodb+srv://Aiswarya:aishumongodb123@cluster0.1btyusi.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true})
+
  
 
 var app= express()
@@ -20,17 +23,54 @@ app.get('/',(req,res)=>{
 app.post('/login',(req,res)=>{
 
     var loginObject=new loginModel(req.body)
-    res.json(loginObject)
 
-
-    
+    loginObject.save(
+        (error)=>{
+            if(error){
+                res.send(error)
+            }else{
+                res.json({'ststus':"success"})
+            }
+        }
+    )
 })
 app.post('/profile',(req,res)=>{
     var profileObject=new profileModel(req.body)
-    res.json(profileObject)
+   
+    profileObject.save(
+        (error)=>{
+            if(error){
+                res.send(error)
+            }else{
+                res.json({'status':"success"})
+            }
+        }
+    )
+})
 
+app.get('/viewlogin',async(req,res)=>{
+ 
+try{
+    var result= await loginModel.find()
+    res.json(result)
+}
+catch(error){
+    res.send(error)
+}
 
 })
+
+app.get('/viewprofile',async(req,res)=>{
+    try{
+        var result= await profileModel.find()
+        res.json(result)
+    }
+    catch(error){
+        res.send(error)
+    }
+})
+
+
 app.listen(process.env.PORT||3002,()=>{
 
     console.log("Server started at http://localhost:3002/")
